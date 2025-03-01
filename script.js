ffde83982c727632aa6977bbed4616d2f7d188fb
Yashtyagi1504 <166196060+Yashtyagi1504@users.noreply.github.com>
2025-03-02 04:01:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ class Car {
 		this.model = model;
 	}
 	getMakeModel(){
-		return `${this.maker} ${this.modil}`
+		return `${this.make} ${this.model}`
 	}
 	
 }
@@ -20,9 +20,14 @@ class SportsCar extends Car{
 	}
 }
 
+// Create a new SportsCar instance
 const car = new SportsCar("Ferrari", "Testarossa", 200);
+
+// Call getMakeModel() to get the make and model of the car
 console.log(car.getMakeModel()); // Output: Ferrari Testarossa
-console.log(car.getTopSpeed()); 
+
+// Call getTopSpeed() to get the top speed of the sports car
+console.log(car.getTopSpeed()); // Output: 200
 
 // Do not change the code below
 window.Car = Car;
